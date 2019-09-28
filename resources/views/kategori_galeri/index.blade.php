377358f245d3b0721f26756d46ca0dea56b3aa9c
suhestihandayani <suhestihandayani18@gmail.com>
2019-09-28 21:05:35 +0800

    update

--- resources/views/kategori_galeri/index.blade.php
+++ resources/views/kategori_galeri/index.blade.php
@@ -1,30 +1,37 @@
-<html>
+@extends('layouts.app')
 
-<head>
-<title>Kategori Galeri</title>
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">List Kategori Galeri</div>
 
-<body>
-<table border="1">
-<tr>
-<td>ID</td>
-<td>Nama</td>
-<td>Users Id</td>
-<td>Create</td>
-</tr>
+                <div class="card-body">
+                    
+                <table border="1">
+                <tr>
+                <td>ID</td>
+                <td>Nama</td>
+                <td>Users Id</td>
+                <td>Create</td>
+                </tr>
 
-@foreach($listKategoriGaleri as $item)
+                @foreach($listKategoriGaleri as $item)
 
-<tr>
-<td>{!!$item->id!!}</td>
-<td>{!!$item->nama!!}</td>
-<td>{!!$item->users_id!!}</td>
-<td>{!!$item->created_at!!}</td>
-</tr>
+                <tr>
+                <td>{!!$item->id!!}</td>
+                <td>{!!$item->nama!!}</td>
+                <td>{!!$item->users_id!!}</td>
+                <td>{!!$item->created_at!!}</td>
+                </tr>
 
-@endforeach
-</table>
+                @endforeach
+                </table>
 
-</body>
-
-</html>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/kategori_pengumuman/index.blade.php
+++ resources/views/kategori_pengumuman/index.blade.php
@@ -1,30 +1,36 @@
-<html>
+@extends('layouts.app')
 
-<head>
-<title>Kategori Pengumuman</title>
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">List Kategori Pengumuman</div>
 
-<body>
-<table border="1">
-<tr>
-<td>ID</td>
-<td>Nama</td>
-<td>Users Id</td>
-<td>Create</td>
-</tr>
+                <div class="card-body">
+                   
+                <table border="1">
+                <tr>
+                <td>ID</td>
+                <td>Nama</td>
+                <td>Users Id</td>
+                <td>Create</td>
+                </tr>
 
-@foreach($listKategoriPengumuman as $item)
+                @foreach($listKategoriPengumuman as $item)
 
-<tr>
-<td>{!!$item->id!!}</td>
-<td>{!!$item->nama!!}</td>
-<td>{!!$item->users_id!!}</td>
-<td>{!!$item->created_at!!}</td>
-</tr>
+                <tr>
+                <td>{!!$item->id!!}</td>
+                <td>{!!$item->nama!!}</td>
+                <td>{!!$item->users_id!!}</td>
+                <td>{!!$item->created_at!!}</td>
+                </tr>
 
-@endforeach
-</table>
-
-</body>
-
-</html>
+                @endforeach
+                </table>                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
